354eda7c1e3add29f1d0d54262862717d0aeefca
Big-Jonny1 <jdgswim@gmail.com>
2018-07-06 01:03:22 -0400

    Not Chris Final Project Update
    I fixed some of the small details like your GameObject organization, and naming.
    I also added tags and a tag check on the red spots so that only they damage you
    I added a timer that decreases your HP every second, making you have to rush
    And I cleaned up your code a little
    And finally edited your UI a little to show your HP easier

--- angryTest/Assets/Scripts/PlayerController.cs
+++ angryTest/Assets/Scripts/PlayerController.cs
@@ -3,17 +3,18 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System;
 
 public class PlayerController : MonoBehaviour
 {
     int hp = 100;
-    bool dmgCHeck = false;
     Vector3 startingLevel1 = new Vector3(16, 8, -35);
     Vector3 startingLevel2 = new Vector3(-93, 60, 130);
     Vector3 startingLevel3 = new Vector3(-125, 1, 125);
     public Text Health;
     Scene currentScene;
     string sceneName;
+    float timer;
 
 
     // Use this for initialization
@@ -23,38 +24,30 @@ public class PlayerController : MonoBehaviour
         sceneName = currentScene.name;
         // Retrieve the name of this scene.
 
-        if (sceneName == "maze")
-        {
-            transform.position = startingLevel1;
-        }
-        else if (sceneName == "maze 2")
-        {
-            transform.position = startingLevel2;
-        }
-        else if (sceneName == "maze 3")
-        {
-            transform.position = startingLevel3;
-        }
+        Restart(); //I made a Function at the bottom that will automatically check and restart your position
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
         Health.text = "Health is " + hp;
+
+        timer = timer + Time.deltaTime; //this adds the miliseconds that have passed every frame together, giving you a realtime timer
+        if (Math.Round(timer, 1) >= 1)
+        {                            //This is rounding off the time to only a single digit, and then checks to see when "1" second has passed
+            hp--; //Lowers hp by 1 every second
+            timer = 0; //this is reset so that the If statement can continuously check to see if "1" second has gone by
+        }
+
 		if (transform.position.y < -10)
         {
-            if (sceneName == "maze")
-            {
-                transform.position = startingLevel1;
-            }
-            else if (sceneName == "maze 2")
-            {
-                transform.position = startingLevel2;
-            }
-            else if (sceneName == "maze 3")
-            {
-                transform.position = startingLevel3;
-            }
+            Restart();
+        }
+
+        if (hp <= 0)
+        {
+            Restart();
+            SceneManager.LoadScene("maze");
         }
 	}
 
@@ -75,23 +68,26 @@ public class PlayerController : MonoBehaviour
                 SceneManager.LoadScene("maze");
             }
         }
-        hp = hp - 10;
-        if (hp <= 0)
+        else if (other.gameObject.tag == "red") //I added a check so that only the red stuff will hurt you
         {
-            if (sceneName == "maze")
-            {
-                transform.position = startingLevel1;
-            }
-            else if (sceneName == "maze 2")
-            {
-                transform.position = startingLevel2;
-            }
-            else if (sceneName == "maze 3")
-            {
-                transform.position = startingLevel3;
-            }
-            hp = 100;
-        }      
+            hp = hp - 10;
+        }
+    }
+
+    void Restart() // I added this so that it could clean up the code a little bit
+    {
+        if (sceneName == "maze")
+        {
+            transform.position = startingLevel1;
+        }
+        else if (sceneName == "maze 2")
+        {
+            transform.position = startingLevel2;
+        }
+        else if (sceneName == "maze 3")
+        {
+            transform.position = startingLevel3;
+        }
     }
 
 }
